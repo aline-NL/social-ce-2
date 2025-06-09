2d2668cc404233bdc43250094c804c1df92a3d96
aline-NL <aline@newlever.ai>
2025-06-09 13:26:16 -0300

    Ajustes na configuração do Gunicorn e WSGI para o Render

---
+++ wsgi.py
@@ -0,0 +1,17 @@
+"""
+WSGI config for the project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+"""
+import os
+import sys
+
+# Adiciona o diretório do projeto ao path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+# Configura o módulo de settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# Importa a aplicação WSGI do Django
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_()
