ec683be42f0736b8a5a544e945100ca297d9299c
aline-NL <aline@newlever.ai>
2025-06-09 13:33:27 -0300

    Correção do comando gunicorn e configuração do Render

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,16 +1,18 @@
+
 """
-WSGI config for core project.
+WSGI config for the project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Adiciona o diretório do projeto ao path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
+# Configura o módulo de settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-application = get_wsgi_application()
+# Importa a aplicação WSGI do Django
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_()
